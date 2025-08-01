ff9cf18fd07372f863aacadd71054636c59fab71
gff111 <247105603@qq.com>
2025-08-01 20:49:50 +0800

    增大bs到8

--- run_script/train_script/run_train_stage2_terediff_chinese.sh
+++ run_script/train_script/run_train_stage2_terediff_chinese.sh
@@ -1,3 +1,3 @@
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=3 accelerate launch train.py \
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0,1 accelerate launch train.py \
     --config configs/train/train_stage2_terediff_chinese.yaml \
     --config_testr testr/configs/TESTR/TESTR_R_50_Polygon_Chinese.yaml
--- run_script/val_script/run_val_terediff_chinese_patches.sh
+++ run_script/val_script/run_val_terediff_chinese_patches.sh
@@ -1,4 +1,4 @@
 
-CUDA_VISIBLE_DEVICES=3 accelerate launch val_patches.py         --config configs/val/val_terediff_patches.yaml \
+CUDA_VISIBLE_DEVICES=2 accelerate launch val_patches.py         --config configs/val/val_terediff_patches.yaml \
                                                         --config_testr testr/configs/TESTR/TESTR_R_50_Polygon_Chinese.yaml \
 
